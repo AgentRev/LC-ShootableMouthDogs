0eac58e2c712170b22b29352fe20cbcc1cec04b3
AgentRev <a456197@gmail.com>
2024-01-07 13:24:02 -0500

    Initial release

---
+++ README.md
@@ -0,0 +1,3 @@
+# ShootableMouthDogs
+
+Fixes bug that prevented eyeless mouthdogs from being killed with the shotgun in Lethal Company. Client-side, no host requirement. Vanilla damage unchanged.
---
+++ ShootableMouthDogs/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace ShootableMouthDogs;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public void Awake()
+    {
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} v{PluginInfo.PLUGIN_VERSION} loaded!");
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+    }
+}
+
+[HarmonyPatch(typeof(Physics))]
+public class PhysicsPatch
+{
+    // Filters out NavMeshAgent hits from SphereCastNonAlloc, since they block gun damage don't really serve a purpose for raycast detection.
+    [HarmonyPatch(nameof(Physics.SphereCastNonAlloc))]
+    [HarmonyPatch(new Type[] { typeof(Ray), typeof(float), typeof(RaycastHit[]), typeof(float), typeof(int), typeof(QueryTriggerInteraction) })]
+    static void Postfix(RaycastHit[] results, int layerMask, ref int __result)
+    {
+        if ((layerMask & 524288) == 0) return; // abort if layerMask doesn't include the one from ShootGun's SphereCastNonAlloc
+
+        int hitCount = 0;
+
+        for (int i = 0; i < __result; i++)
+        {
+            if (results[i].transform?.GetComponent<NavMeshAgent>() == null)
+            {
+                if (i > hitCount) results[hitCount] = results[i]; // RaycastHit struct shallow copy
+                hitCount++;
+            }
+        }
+
+        __result = hitCount;
+    }
+}
